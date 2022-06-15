b2ff5ddad2f9d622a6c12feac14902f539b2b33a
EmreOzguler <yunus.e.o@gmail.com>
2022-06-15 15:38:47 +0300

    Düzeltiler yapıldı

--- JavaOdevleri/src/BkiHesaplama/BkiHesaplama.java
+++ JavaOdevleri/src/BkiHesaplama/BkiHesaplama.java
@@ -2,21 +2,27 @@ package BkiHesaplama;
 
 import java.util.Scanner;
 
-public class BkiHesaplama {
-
-	public static void main(String[] args) {
+	public class BkiHesaplama{
+ 
+		public static void main(String []argh){
+	    	Scanner input = new Scanner(System.in);
+	    	
+			System.out.print("Boyunuzu metre cinsinden giriniz = ");
+			double boy = input.nextDouble();
 		
-		Scanner input = new Scanner(System.in);
-	
-		System.out.print("Boyunuzu metre cinsinden giriniz = ");
-		double boy = input.nextDouble();
-	
-		System.out.print("Kilonuzu giriniz = ");
-		double kilo = input.nextDouble();
-	
-		double bki = kilo/(Math.pow(boy, 2));
-		System.out.println("Beden Kitle indeksiniz : " + bki);
-	
-	input.close();
+			System.out.print("Kilonuzu giriniz = ");
+			double kilo = input.nextDouble();
+		
+			double bki = kilo/(Math.pow(boy, 2));
+			System.out.println("Beden Kitle indeksiniz : " + bki);
+		
+		input.close();
+
+        }
 	}
-}
+
+
+
+
+	
+	
